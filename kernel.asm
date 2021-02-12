6152b7e90c5095411c1632cc737f9c751cc5911e
captain-rainbow <67162230+captain-rainbow@users.noreply.github.com>
2021-02-12 11:44:16 +0100

    Implement printstring function (BROKEN)

--- kernel.asm
+++ kernel.asm
@@ -1,13 +1,23 @@
 ; Made by Victor Mout
-mainloop:
-	jmp readchar
 
+teststring 		db 'bruh', 0x00
+mainloop:
+	jmp printstring
 
 printchar:
 	mov ah, 0x0e ; Enter teletype mode
-	int 0x10
-        jmp mainloop
+        int 0x10 
+	jmp mainloop
+
+printstring:
+	mov si, teststring
+	mov ah, 0x0e
 	
+	lodsb
+	
+	int 0x10
+	jmp printstring
+
 readchar:
 	mov ah, 01h ; Test key
 	int 16h
@@ -16,10 +26,13 @@ readchar:
 	int 16h
 
 	jmp printchar
+	        
+return:
+	jmp mainloop 
 
-jmp mainloop 
 
 times 510-($-$$) db 0 ; Repeat db 0 512 times
 db 0x55, 0xaa ; End boot sector 0xaa
+
 
 
