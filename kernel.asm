4225ca8774a8086964a227096bbf2d12b74d2997
captain-rainbow <67162230+captain-rainbow@users.noreply.github.com>
2021-02-12 09:50:26 +0100

    Kernel level input functionality

--- kernel.asm
+++ kernel.asm
@@ -5,12 +5,8 @@ mainloop:
 
 printchar:
 	mov ah, 0x0e ; Enter teletype mode
-	inc bx
 	int 0x10
         jmp mainloop
-test:
-        mov al, 'A'
-        jmp printchar
 	
 readchar:
 	mov ah, 01h ; Test key
@@ -19,9 +15,7 @@ readchar:
  	mov ah, 00h ; Get key
 	int 16h
 
-	cmp al, 62 ; Compare key to Escape in hex
-        je test ; If so, start test print subroutine
-	jne mainloop ; If not, jump to mainloop
+	jmp printchar
 
 jmp mainloop 
 
