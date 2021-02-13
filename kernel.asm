8580776881d1bc11e58a62d3e2ae9693dcd4708f
captain-rainbow <67162230+captain-rainbow@users.noreply.github.com>
2021-02-13 13:41:52 +0100

    Add printstring function, and implement 16 bit+offset

--- kernel.asm
+++ kernel.asm
@@ -1,38 +1,39 @@
+bits 16
+org 0x7c00
 ; Made by Victor Mout
 
-teststring 		db 'bruh', 0x00
+jmp printstring
 mainloop:
-	jmp printstring
 
 printchar:
-	mov ah, 0x0e ; Enter teletype mode
-        int 0x10 
-	jmp mainloop
+        mov ah, 0x0e ; Enter teletype mode
+        int 0x10
+        jmp mainloop
+
 
 printstring:
-	mov si, teststring
-	mov ah, 0x0e
-	
-	lodsb
-	
-	int 0x10
-	jmp printstring
+        mov si, version
+        .loop:
+          mov ah, 0x0e
+          lodsb
+          cmp al, 0x00
+          je return
+          int 0x10
+          jmp .loop
 
 readchar:
-	mov ah, 01h ; Test key
-	int 16h
+        mov ah, 01h ; Test key
+        int 16h
 
- 	mov ah, 00h ; Get key
-	int 16h
+        mov ah, 00h ; Get key
+        int 16h
 
-	jmp printchar
-	        
+        jmp printchar
+
 return:
-	jmp mainloop 
+        ret
 
-
+version         db 'VictorOS 0.1 (Dev)', 0x00
 times 510-($-$$) db 0 ; Repeat db 0 512 times
 db 0x55, 0xaa ; End boot sector 0xaa
-
-
 
